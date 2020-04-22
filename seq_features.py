12b6e51c235fe133876d8a6470da313ae6faa356
ewatso11 <63322695+ewatso11@users.noreply.github.com>
2020-04-22 16:36:33 -0700

    Add files via upload

---
+++ seq_features.py
@@ -0,0 +1,26 @@
+import bootcamp_utils
+
+def number_negatives(seq):
+    """Number of negative residues a protein sequence"""
+    # Convert sequence to upper case
+    seq = seq.upper()
+
+    # Check for a valid sequence
+    for aa in seq:
+        if aa not in bootcamp_utils.aa.keys():
+            raise RuntimeError(aa + ' is not a valid amino acid.')
+
+    # Count E's and D's, since these are the negative residues
+    return seq.count('E') + seq.count('D')
+
+def number_positives(seq):
+    """Number of positive residues a protein sequence"""
+    # Convert sequence to upper case
+    seq = seq.upper()
+
+    # Check for a valid sequence
+    for aa in seq:
+        if aa not in bootcamp_utils.aa.keys():
+            raise RuntimeError(aa + ' is not a valid amino acid.')
+
+    return seq.count('R') + seq.count('K') + seq.count('H')
